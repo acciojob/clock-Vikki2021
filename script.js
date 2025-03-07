6bef0fbb0b7a38ee413c00b89d240215eba86464
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-07 22:37:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let time = document.getElementById('timer');
+let currTime = new Date()
+
+function cb() {
+	currTime = new Date();
+	time.innerHTML = currTime.toLocaleString();
+}
+
+setInterval(cb,1000);
